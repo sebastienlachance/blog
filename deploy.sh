5965a642e35cae3af7429b5de8f899d2bc2b3000
Sebastien Lachance <sebastien.lachance@gmail.com>
2012-05-10 10:04:53 -0400

    new post

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+bundle exec rake generate
+echo "Adding files"
+git add -A
+echo "Committing"
+git commit -am "new post"
+echo "Pushing to github"
+git push github
+echo "Pushing to Heroku
+git push heroku
